1642c8b4fd97bfb535bb018dfd63a80c5370b6d4
kiran <142571315+BlueFlareKiran@users.noreply.github.com>
2026-03-18 13:30:52 +0530

    test2

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors({
 }))
 app.use(express.json())
 
-app.use('api/seats', seatRoutes)
+app.use('/api/seats', seatRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
